f1bf984d5ede5d420895244ba97b86bfdf420fa0
WIN-7NR2N28JV5M\traininguser <namito.asano@skywill.jp>
2026-06-02 14:15:37 +0900

    first commit

---
+++ src/main/java/com/pfc_navi/pfc_navi/PfcNaviApplication.java
@@ -0,0 +1,13 @@
+package com.pfc_navi.pfc_navi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfcNaviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfcNaviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pfc_navi/pfc_navi/PfcNaviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfc_navi.pfc_navi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfcNaviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
