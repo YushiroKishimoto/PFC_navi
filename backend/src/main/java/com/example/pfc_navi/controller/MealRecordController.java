37d28936eb6be13fd606401fb423d1a6b1e319c7
WIN-937N7CTGEB3\traininguser <satoshi.ishimura@skywill.jp>
2026-06-09 14:49:02 +0900

    スキーマ修正ver1

--- backend/src/main/java/com/example/pfc_navi/controller/MealRecordController.java
+++ backend/src/main/java/com/example/pfc_navi/controller/MealRecordController.java
@@ -20,14 +20,15 @@ public class MealRecordController {
     }
 
     //開発中のログイン認証のために仮でuserID_1を保持する
-    private Integer getUserId(Principal principal) {
-        if (principal == null) {
+    //private Integer getUserId(Principal principal) {
+    //    if (principal == null) {
             // 開発用の仮ユーザーID
-            return 1;
-        }
+    //        return 1;
+    //    }
 
-        return Integer.parseInt(principal.getName());
-    }
+    //    return Integer.parseInt(principal.getName());
+    //}
+    
 
     @PostMapping
     public ApiResponse<?> createMealRecord(
@@ -35,9 +36,8 @@ public class MealRecordController {
             Principal principal) {
         try {
             //結合時にファイルここを追加し、上記のこめんとあうとを削除する
-            //Integer userId = Integer.parseInt(principal.getName());
+            Integer userId = Integer.parseInt(principal.getName());
 
-            Integer userId = getUserId(principal);
             return ApiResponse.success("食事記録を保存しました。", mealRecordService.createMealRecord(request, userId));
         } catch (IllegalArgumentException e) {
             return ApiResponse.validationError(e.getMessage(), Map.of());
@@ -49,8 +49,8 @@ public class MealRecordController {
             @RequestParam LocalDate date,
             Principal principal) {
         try {
-            //Integer userId = Integer.parseInt(principal.getName());
-            Integer userId = getUserId(principal);
+            Integer userId = Integer.parseInt(principal.getName());
+            
             return ApiResponse.success("取得成功", mealRecordService.getMealRecordsByDate(date, userId));
         } catch (IllegalArgumentException e) {
             return ApiResponse.validationError(e.getMessage(), Map.of());
@@ -62,8 +62,8 @@ public class MealRecordController {
             @PathVariable Integer id,
             Principal principal) {
         try {
-            //Integer userId = Integer.parseInt(principal.getName());
-            Integer userId = getUserId(principal);
+            Integer userId = Integer.parseInt(principal.getName());
+            
             return ApiResponse.success(
                     "食事記録を削除しました。",
                     mealRecordService.deleteMealRecord(id, userId));
